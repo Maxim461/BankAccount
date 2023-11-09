15b8e64fa57ac7f695e0120dc38b5af574f726ae
maksli <Max2004lee@mail.ru>
2023-11-09 14:32:48 +0300

    my changes x

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -8,31 +8,38 @@ class Program
         Account AccountOne = new Account(593481, "Зуев Александор Максимович", 0);
         Account AccountTwo = new Account(315317, "Кириллова Виктория Викторовна", 0);
 
+        Account AccountSelection = null;
 
         while (true)
         {
-            Console.WriteLine("Введите банк");
-            int AccountSelectionVar = int.Parse(Console.ReadLine());
-
-            Account AccountSelection;
-            
-                if (AccountSelectionVar == 1)
+            if (AccountSelection == null)
+            {
+                Console.WriteLine("Выберите банк (1 или 2)");
+                int ChoosingCarIfElse = int.Parse(Console.ReadLine());
+                if (ChoosingCarIfElse == 0)
+                {
+                    break;
+                }
+                else if (ChoosingCarIfElse == 1)
                 {
                     AccountSelection = AccountOne;
                 }
-                else if (AccountSelectionVar == 2)
+                else if (ChoosingCarIfElse == 2)
                 {
                     AccountSelection = AccountTwo;
                 }
-            else {continue;}
-                
-            
+                else
+                {
+                    Console.WriteLine("Вы не правильно выбрали банк.\nВведите 1 или 2.");
+                }
+            }
 
             Console.WriteLine("1 - Показать информацию о счете;");
             Console.WriteLine("2 - Положить на счет;");
             Console.WriteLine("3 - Снять со счета;");
             Console.WriteLine("4 - Взять всю сумму;");
-            Console.WriteLine("5 - Перенести сумму с одного счета на другой.");
+            Console.WriteLine("5 - Перенести сумму с одного счета на другой;");
+            Console.WriteLine("6 - Сменить банк;");
 
             int Button = int.Parse(Console.ReadLine());
             switch (Button)
@@ -73,6 +80,10 @@ class Program
                     Console.WriteLine("Введите сумму для перевода:");
                     double TransferAmount = double.Parse(Console.ReadLine());
                     AccountSelection.Transfer(RecipientAccount, TransferAmount);
+                    break;
+
+                case 6:
+                    AccountSelection = null;
                     break;
             }
         }
